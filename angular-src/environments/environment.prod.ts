a5d09b6f6dbe8c418ad7f015047d62062774c0f0
Ben Coleman <benc.uk@gmail.com>
2017-11-09 17:52:38 +0000

    Renamed from Jimi to MicroSurvey and couple of bug fixes

--- angular-src/environments/environment.prod.ts
+++ angular-src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //api_endpoint: 'http://feedback-api.westus2.cloudapp.azure.com/api'
-  api_endpoint: 'http://localhost:4000/api',
-  secured: false
+  api_endpoint: 'https://micro-survey-api.azurewebsites.net/api',
+  //api_endpoint: 'http://localhost:4000/api',
+  secured: true
 };
