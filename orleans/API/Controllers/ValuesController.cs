a6c523530381a83ccc0f700e6b285e43b60db0aa
Ben Coleman <becolem@microsoft.com>
2018-02-26 15:52:37 +0000

    Added Orleans app, very early first draft

---
+++ orleans/API/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrainInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using Orleans;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private IClusterClient client;
+        private readonly ILogger logger;
+        
+        public ValuesController(IClusterClient client, ILogger<ValuesController> logger)
+        {
+            this.client = client;
+            this.logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            logger.LogInformation("GET ALL");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<string> Get(int id)
+        {
+            logger.LogInformation($"GET {id}");
+            await ConnectClientIfNeeded();
+            var grain = this.client.GetGrain<IValueGrain>(id);
+            return await grain.GetValue();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody]string value)
+        {
+            logger.LogInformation($"PUT {id} = {value}");
+            await ConnectClientIfNeeded();
+            var grain = this.client.GetGrain<IValueGrain>(id);
+            await grain.SetValue(value);
+        }
+
+        private async Task ConnectClientIfNeeded()
+        {
+            if (!this.client.IsInitialized)
+                await this.client.Connect();
+        }
+    }
+}
---
+++ orleans/Silo/Program.cs
@@ -0,0 +1,70 @@
+﻿using Grains;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Hosting;
+using Orleans.Runtime.Configuration;
+using System;
+using System.Net;
+using System.Runtime.Loader;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Silo
+{
+    public class Program
+    {
+        private static ISiloHost silo;
+        private static readonly ManualResetEvent siloStopped = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            // TODO This will not be needed in RC
+            var config = new ClusterConfiguration();
+
+            // Added BC - Get the config from appsettings.json
+            var appSettingsBuilder = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            IConfigurationRoot appSettings = appSettingsBuilder.Build();
+
+            config.Globals.DataConnectionString = appSettings["Orleans:ConnectionString"];
+            config.Globals.ClusterId = appSettings["Orleans:ClusterId"];
+
+            config.Globals.LivenessType = GlobalConfiguration.LivenessProviderType.AzureTable;
+            config.Globals.ReminderServiceType = GlobalConfiguration.ReminderServiceProviderType.AzureTable;
+            config.Globals.FastKillOnCancelKeyPress = true;
+            config.Defaults.Port = 11111;
+            config.Defaults.ProxyGatewayEndpoint = new IPEndPoint(IPAddress.Any, 30000);
+            silo = new SiloHostBuilder()
+                .UseConfiguration(config)
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(ValueGrain).Assembly).WithReferences())
+                .ConfigureLogging(builder => builder.SetMinimumLevel( (LogLevel)appSettings.GetValue<int>("Orleans:LogLevel") ).AddConsole())
+                .Build();
+
+            Task.Run(StartSilo);
+
+            AssemblyLoadContext.Default.Unloading += context =>
+            {
+                Task.Run(StopSilo);
+                siloStopped.WaitOne();
+            };
+
+            siloStopped.WaitOne();
+        }
+
+        private static async Task StartSilo()
+        {
+            await silo.StartAsync();
+            Console.WriteLine("Silo started");
+        }
+
+        private static async Task StopSilo()
+        {
+            await silo.StopAsync();
+            Console.WriteLine("Silo stopped");
+            siloStopped.Set();
+        }
+    }
+}
