324b33f8a2e17ce9a72910c9c26c509be0b8603c
Ben Coleman <becolem@microsoft.com>
2018-02-26 15:52:37 +0000

    Added Orleans app, very early first draft

---
+++ orleans/API/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using GrainInterfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Orleans;
+using Orleans.Hosting;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Console.WriteLine($"#################");
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<IClusterClient>(CreateClusterClient);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+
+        private IClusterClient CreateClusterClient(IServiceProvider serviceProvider)
+        {
+            Console.WriteLine($"Using ClusterId = {this.Configuration["Orleans:ClusterId"]}");
+            Console.WriteLine($"Using ConnectionString = {this.Configuration["Orleans:ConnectionString"]}");
+            
+            var client = new ClientBuilder()
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IValueGrain).Assembly))
+                .ConfigureClusterClient(options => options.ClusterId = this.Configuration["Orleans:ClusterId"])
+                .UseAzureTableGatewayListProvider(options => options.ConnectionString = this.Configuration["Orleans:ConnectionString"])
+                .Build();
+            return client;
+        }
+    }
+}
