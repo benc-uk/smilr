c3bbf68ad6a656cb771f774de87713d5f20ec71a
Ben Coleman <becolem@microsoft.com>
2018-02-28 10:40:56 +0000

    Tasks sould work on powershell and WSL. App Svc deploy script improved

--- etc/deploy-appsvc/deploy-frontend.ps1
+++ etc/deploy-appsvc/deploy-frontend.ps1
@@ -1,18 +1,30 @@
 #
-# This script is designed to deploy the frontend to an  
-# existing Azure App Service instance 
+# This script is designed to deploy the frontend in prod mode to an existing Azure App Service instance 
+# This requires Node.js and the Angular CLI installed on Windows, not inside WSL
 #
 
 # CHANGE THESE!
-$appService = "smilr-live"
-$appSvcUser = "bcdeployer"
+param (
+    [string]$appService = "smilr-live",
+    [string]$deployUser = "bcdeployer"
+)
 
 # Don't change these
 $tempDir = ".\tmp_frontend\"
 $zipName = "tmp_frontend.zip"
 
-echo "### Building Angular app for deployment"
-iex "ng build --prod"
+echo "### Building Angular app for deployment to: ${appService}"
+try {
+    iex "ng build --prod"
+} catch {
+    echo "### Error running Angular build, likely you don't have Angular CLI installed"
+}
+if(Test-Path "..\..\dist") {
+    echo "### I found a dist folder in the project root, so will carry on and use that!"    
+} else {
+    echo "### Wasn't able to ``run ng`` build and no dist folder was found, I can't carry on, sorry bye!"  
+    exit  
+}
 
 echo "### Bundling Node server with Angular app"
 rmdir -fo -r $tempDir
@@ -28,10 +40,11 @@ rmdir $zipName
 Compress-Archive "$tempDir*" -DestinationPath $zipName
 
 # All this crap is to work around lack of HTTP basic auth in Invoke-WebRequest 
+# I should have written this in bash!
 $SecurePassword = Read-Host -AsSecureString  "### App Service deployment password"
 $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)            
 $pass = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)  
-$pair = "$($appSvcUser):$($pass)"
+$pair = "$($deployUser):$($pass)"
 $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
 $basicAuthValue = "Basic $encodedCreds"
 $Headers = @{
