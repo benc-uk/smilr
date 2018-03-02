f9154b34de5d42971774397c0266e68209621a14
Ben Coleman <becolem@microsoft.com>
2018-03-02 17:38:53 +0000

    Huge restructure of repo. Part 2: App Service scripting hell

--- azure/appservice/deploy-frontend.ps1
+++ azure/appservice/deploy-frontend.ps1
@@ -10,16 +10,25 @@ param (
 )
 
 # Don't change these
-$tempDir = ".\tmp_frontend\"
-$zipName = "tmp_frontend.zip"
+$tempDir = ".\azure\appservice\tmp\"
+$zipName = ".\azure\appservice\tmp.zip"
+$angularDir = ".\angular"
 
-echo "### Building Angular app for deployment to: ${appService}"
+
+if((Test-Path "azure") -And (Test-Path "angular")) {
+    echo "### Building Angular app for deployment to: ${appService}"
+} else {
+    echo "### YOU MUST RUN THIS FROM THE PROJECT ROOT, BYE!"
+    exit
+}
+
 try {
-    iex "ng build --prod"
+    #Start-Process -FilePath "node" -ArgumentList "c:\dev\microservices-demoapp\angular\node_modules\@angular\cli\bin\ng build","--prod" -WorkingDirectory $angularDir
 } catch {
     echo "### Error running Angular build, likely you don't have Angular CLI installed"
 }
-if(Test-Path "..\..\dist") {
+
+if(Test-Path $angularDir\dist) {
     echo "### I found a dist folder in the project root, so will carry on and use that!"    
 } else {
     echo "### Wasn't able to ``run ng`` build and no dist folder was found, I can't carry on, sorry bye!"  
@@ -27,16 +36,21 @@ if(Test-Path "..\..\dist") {
 }
 
 echo "### Bundling Node server with Angular app"
-rmdir -fo -r $tempDir
+try {
+    rmdir -fo -r $tempDir
+} catch {}
+
 mkdir $tempDir
-cp -r ..\..\dist\* -Destination $tempDir
-cp ..\..\service-frontend\package.json -Destination $tempDir
-cp ..\..\service-frontend\package-lock.json -Destination $tempDir
-cp ..\..\service-frontend\server.js -Destination $tempDir
-cp ..\..\service-frontend\.deployment -Destination $tempDir
+cp -r $angularDir\dist\* -Destination $tempDir
+cp node\frontend\package.json -Destination $tempDir
+cp node\frontend\package-lock.json -Destination $tempDir
+cp node\frontend\server.js -Destination $tempDir
+cp node\frontend\.deployment -Destination $tempDir
 
 echo "### Zipping everything ready for deployment"
-rmdir $zipName
+try { 
+    rm $zipName
+} catch {}
 Compress-Archive "$tempDir*" -DestinationPath $zipName
 
 # All this crap is to work around lack of HTTP basic auth in Invoke-WebRequest 
