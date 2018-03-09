522acbe6d57d66cd56e9ad209c1651bf2ad40cd8
Ben Coleman <becolem@microsoft.com>
2018-03-09 15:32:37 +0000

    Moved mobile sidenav into a proper component no more getElementById!

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -4,32 +4,16 @@ import { UserService } from './admin/user.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app'; 
   
-  env = environment;
-  userService: UserService;
+  public env = environment;
+  public userService: UserService;
+  @ViewChild('sideNav') public sideNav;
 
   constructor(us: UserService) {
     this.userService = us;
   }
-  
-  openNav() {
-    document.getElementById("mobileNav").style.width = "50%";
-  }
-
-  closeNav() {
-    document.getElementById("mobileNav").style.width = "0";
-  }
-
-  toggleNav() {
-    var wid = document.getElementById("mobileNav").style.width;
-    if(wid.length == 0 || wid == "0" || wid == "0px")
-      document.getElementById("mobileNav").style.width = "50%";
-    else
-      document.getElementById("mobileNav").style.width = "0";
-  }  
 }
