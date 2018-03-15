cc94a035524b401e583e8b8bcd5e302d81d9db3a
Ben Coleman <becolem@microsoft.com>
2018-03-15 22:43:09 +0000

    Mongo version done

--- node/data-api/routes/api-feedback.js
+++ node/data-api/routes/api-feedback.js
@@ -1,38 +1,23 @@
 const express = require('express');
 const routes = express.Router();
-const uuidv4 = require('uuid/v4');
-const os = require('os');
-const fs = require('fs');
-
-var dataAccess = require('../lib/data-access');
+const utils = require('../lib/utils');
 
 // Routes for feedback API 
 
 routes
 .get('/api/feedback/:eventid/:topicid', function (req, res, next) {
   res.type('application/json');
-  dataAccess.listFeedbackForEventTopic(req.params.eventid, parseInt(req.params.topicid))
-    .then(d => res.send(d))
-    .catch(e => sendError(res, e));
+  res.app.get('data').listFeedbackForEventTopic(req.params.eventid, parseInt(req.params.topicid))
+    .then(data => utils.sendData(res, data))
+    .catch(err => utils.sendError(res, err));
 })
 
 .post('/api/feedback', function (req, res, next) {
   let feedback = req.body;
   res.type('application/json');
-  dataAccess.createFeedback(feedback)
-    .then(d => res.send(d))
-    .catch(e => sendError(res, e));
+  res.app.get('data').createFeedback(feedback)
+    .then(data => utils.sendData(res, data.ops[0]))
+    .catch(err => utils.sendError(res, err));
 })
-
-//
-// Try to send back the underlying error code and message
-//
-function sendError(res, err) {
-  console.log(`### Error with feedback API ${JSON.stringify(err)}`); 
-  let code = 500;
-  if(err.code > 1) code = err.code;
-  res.status(code).send(err);
-  return;
-}
 
 module.exports = routes;
