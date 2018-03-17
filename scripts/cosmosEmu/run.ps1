a72f7efcbdeb295853da91c1f17d5c028ee117d7
Ben Coleman <becolem@microsoft.com>
2018-03-17 15:36:10 +0000

    No longer required

--- scripts/cosmosEmu/run.ps1
+++
@@ -1,4 +0,0 @@
-#
-# Simple helper script to run Cosmos DB Emulator with network access and default key
-#
-& "C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe" /AllowNetworkAccess /Key=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==
