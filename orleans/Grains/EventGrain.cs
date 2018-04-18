d09ad924549088c86455ed06893c5c35da35d1e2
David Gristwood (Microsoft) <davidgri@microsoft.com>
2018-04-18 19:07:21 +0100

    adding feedback to grains

--- orleans/Grains/EventGrain.cs
+++ orleans/Grains/EventGrain.cs
@@ -1,8 +1,11 @@
 using GrainInterfaces;
 using GrainModels;
 using Orleans;
-using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+
+
+
 
 namespace Grains
 {
@@ -12,64 +15,68 @@ namespace Grains
   [Orleans.Providers.StorageProvider(ProviderName = "grain-store")]  // we will us auto persist for event grains
   public class EventGrain : Grain<EventGrainState>, IEventGrain
   {
-    // create / update an event
-    //  the system doesn't really distinguish between the two, just to keep the logic simple
-    //  scenarios include changing the start or endd date, changing the list of topics for that event, etc
-    public async Task Update(string title, string type, string start, string end, TopicAPI[] topics)
-    {
-        // store state
-        State.title = title;
-        State.type = type;
-        State.start = start;
-        State.end = end;
-        State.topics = topics;
+      List<FeedbackGrainState> feebdack;
+
+
+      // create / update an event
+      //  the system doesn't really distinguish between the two, just to keep the logic simple
+      //  scenarios include changing the start or endd date, changing the list of topics for that event, etc
+      public async Task Update(string title, string type, string start, string end, TopicAPI[] topics)
+      {
+          // store state
+          State.title = title;
+          State.type = type;
+          State.start = start;
+          State.end = end;
+          State.topics = topics;
                 
-        return; 
-    }
+          return; 
+      }
 
 
-    // return core info about this event
-    public async Task<EventAPI> Info()
-    {
-      EventAPI info = new EventAPI();
+      // return core info about this event
+      public async Task<EventAPI> Info()
+      {
+          EventAPI info = new EventAPI();
 
-      info.id = this.GetPrimaryKeyString(); // make sure we return this grain key as event id
-      info.title = State.title;
-      info.type = State.type;
-      info.start = State.start;
-      info.end = State.end;
-      info.topics = State.topics;
+          info.id = this.GetPrimaryKeyString(); // make sure we return this grain key as event id
+          info.title = State.title;
+          info.type = State.type;
+          info.start = State.start;
+          info.end = State.end;
+          info.topics = State.topics;
 
-      return info;
-    }
-
-
+          return info;
+      }
 
 
+      // store user feedback for this event
+      public async Task SubmitFeedback(int topicId, int rating, string comment)
+      {
+          // check topic is valid
+          if (topicId > State.topics.Length)
+              return;  // should we surface error?
+
+          // store feedback
+          FeedbackGrainState f = new FeedbackGrainState();
+          f.topicId = topicId;
+          f.rating = rating;
+          f.comment = comment;
+          State.feedback.Add(f);
+
+          return;
+      }
 
 
-    /* 
-    public Task<string> GetTitle()
-    {
-      // Getting it from GrainState, is that bad? I don't think so unless I call base.ReadStateAsync();
-      base.ReadStateAsync();
-      return Task.FromResult(State.title.ToString());
-    }
 
-    public Task SetTitle(string title)
-    {
-      // Getting it from GrainState, is that bad? I don't think so unless I call base.ReadStateAsync();
-      State.title = title;
-      base.WriteStateAsync();
-      return Task.CompletedTask;
-    }
-    */
+      public async Task<FeedbackAPI> GetFeedback(int topicid)
+      {
+          FeedbackAPI f = new FeedbackAPI();
 
-    /*public Task SetValue(string value)
-    {
-      State.value = value;
-      base.WriteStateAsync();
-      return Task.CompletedTask;
-    }*/
+          // TODO - write!
+
+          return f;
+      }
+
   }
 }
