c71a93b7f02a26855d07c026b58731533b32fc7c
Ben Coleman <ben.coleman@microsoft.com>
2018-05-19 18:32:04 +0100

    Bot updated with API calls

--- bot/utils.js
+++ bot/utils.js
@@ -1,9 +1,19 @@
+const request = require('request-promise-native');
 
 class Utils {
 
   // Random int between 0 and (max - 1)
   static getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  static getEvents(filter) {
+    let apiEndpoint = process.env.API_ENDPOINT || 'http://localhost:4000/api';
+    return request(`${apiEndpoint}/events/filter/${filter}`)
+  }
+  
+  static sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 }
 
