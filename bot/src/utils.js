915dded12bec4a29e400e4f4d19399065157b740
Ben Coleman <becolem@microsoft.com>
2018-05-24 17:51:37 +0100

    License update

--- bot/src/utils.js
+++ bot/src/utils.js
@@ -2,6 +2,8 @@
 // Simple static helper functions
 //
 
+const ASSET_DIR = `${__dirname}/../assets`
+
 class Utils {
 
   // Random int between 0 and (max - 1)
@@ -15,6 +17,11 @@ class Utils {
 
   static sendRandomPhrase(session, phrases) {
     // TODO!
+  }
+
+  static getImageSVG(filename) {
+    let svgImg = require('fs').readFileSync(`${ASSET_DIR}/${filename}`);
+    return `data:image/svg+xml;utf8,${svgImg.toString()}`
   }
 }
 
