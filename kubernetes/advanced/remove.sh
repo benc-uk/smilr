f2a826d9a0538f022214452a0f44a75c2fdbc19e
Ben Coleman <becolem@microsoft.com>
2018-12-03 18:21:14 +0000

    Lots of K8S updates & tweaks

--- kubernetes/advanced/remove.sh
+++ kubernetes/advanced/remove.sh
@@ -1,16 +1,14 @@
 printf "\n### Removing MongoDB\n"
-kubectl delete -f mongodb.all.yaml
+kubectl delete -f mongodb.yaml
 
 printf "\n### Removing Data API microservice\n"
-kubectl delete -f data-api.deploy.yaml
-kubectl delete -f data-api.svc.yaml
+kubectl delete -f data-api.yaml
 
 printf "\n### Removing Frontend microservice\n"
-kubectl delete -f frontend.deploy.yaml
-kubectl delete -f frontend.svc.yaml
+kubectl delete -f frontend.yaml
 
 printf "\n### Removing HTTPS ingress\n"
-kubectl delete -f ingress.https.yaml
+kubectl delete -f ingress-https.yaml
 
 printf "\n"
 kubectl get all
