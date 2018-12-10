196f36a0aa39acb9a3dc21ef3a3698927f6e3f60
Ben Coleman <becolem@microsoft.com>
2018-12-10 09:12:58 +0000

    Started on Vue app frontend rewrite

---
+++ vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="md" type="dark" variant="primary">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand :to="{name:'home'}" style="font-size:32px;"><img src="./assets/logo-white.svg" class="logo"> &nbsp;Smilr</b-navbar-brand>
+       <b-collapse is-nav id="nav_collapse">
+          <b-navbar-nav >
+            <b-button size="lg" :to="{name:'events'}">Events</b-button> 
+            &nbsp;&nbsp;
+            <b-button size="lg" :to="{name:'about'}">About</b-button>
+          </b-navbar-nav>
+       </b-collapse>
+    </b-navbar>
+
+    <br/>
+    
+    <b-container>
+      <router-view/>
+    </b-container>
+  </div>
+</template>
+
+<style>
+  .logo {
+    height: 40px;
+  }
+</style>
---
+++ vue/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" >
+    <p>For more information visit website</p>
+    <b-btn variant="primary" href="#">More Info</b-btn>
+  </b-jumbotron>
+
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
