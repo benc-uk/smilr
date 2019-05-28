4456ae9036444c21e419e65884541c97a283d417
Ben Coleman <benc.uk@gmail.com>
2019-05-28 10:35:28 +0100

    Updated info and dates on about page

--- vue/src/components/About.vue
+++ vue/src/components/About.vue
@@ -9,16 +9,17 @@
       <hr/>
 
       <b>Version: {{ ver }} (Vue.js)</b><br/>
-      &copy; Ben Coleman, 2017/2018<br/>
-      Provided under the <a target="_blank" href="https://opensource.org/licenses/MIT">MIT License</a><br/>
-      <b-button variant="primary" href="https://smilr.benco.io" target="_blank">Project details &amp; source code</b-button>
+      &copy; Ben Coleman, 2017 - 2019<br/>
+      Provided under the <a target="_blank" href="https://opensource.org/licenses/MIT">MIT License</a><br/><br/>
+      <b-button size="lg" variant="primary" href="https://smilr.benco.io" target="_blank">Project details &amp; source code (GitHub)</b-button>
       
       <hr/>
 
-      <b>Debug Info:</b><br>
-      API Endpoint: <a :href="apiEndpoint">{{ apiEndpoint }}</a> <br/>
-      Mode: {{ mode }} <br/>
-      Build Info: {{ buildInfo }}
+      <pre>
+<b>API Endpoint:</b> <a :href="apiEndpoint">{{ apiEndpoint }}</a>
+<b>Mode:</b> {{ mode }}
+<b>Build Info:</b> {{ buildInfo }}</pre>
+
     </b-card>
   </div>
 </template>
@@ -57,5 +58,10 @@ export default {
 <style scoped>
 .aboutface {
   width: 100px;
+}
+pre {
+  background-color: rgb(228, 228, 228);
+  padding: 1rem;
+  font-size: 70%;
 }
 </style>
