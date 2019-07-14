2865612138f3cd04a776a147e17e9e0956dbe0f4
Ben Coleman <benc.uk@gmail.com>
2019-07-14 15:41:11 +0100

    b

--- azure/pipelines/test.sh
+++ azure/pipelines/test.sh
@@ -4,6 +4,6 @@ secret=$(cat $HOME/.docker/config.json | jq -r '.auths | with_entries( select(.k
 export _ACR_USER=$(echo $secret | awk -F':' '{print $1}')
 export _ACR_PASSWORD=$(echo $secret | awk -F':' '{print $2}')
 
-echo $_ACR_NAME
-echo $_ACR_USER
-echo $_ACR_PASSWORD
+echo $_ACR_NAME >> o.txt
+echo $_ACR_USER >> o.txt
+echo $_ACR_PASSWORD >> o.txt
