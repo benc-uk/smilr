dd77c64e02fe6e04c9f0a0811bb1da05aaf8fda8
Ben Coleman <benc.uk@gmail.com>
2019-07-14 16:05:38 +0100

    ccc

--- azure/pipelines/test.sh
+++ azure/pipelines/test.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
 
 export _ACR_NAME=$(cat $HOME/.docker/config.json | jq -r '.auths | with_entries( select(.key|contains("azurecr.io")) ) | keys[0]')
 secret=$(cat $HOME/.docker/config.json | jq -r '.auths | with_entries( select(.key|contains("azurecr.io")) ) [].auth' | python -m base64 -d)
 export _ACR_USER=$(echo $secret | awk -F':' '{print $1}')
 export _ACR_PASSWORD=$(echo $secret | awk -F':' '{print $2}')
 
-echo $_ACR_NAME >> o.txt
-echo $_ACR_USER >> o.txt
-echo $_ACR_PASSWORD >> o.txt
+export _S=$(echo $_ACR_NAME | sed "s/.azurecr.io//")
+
+
+echo $_S
+echo $_ACR_NAME
+echo $_ACR_USER
+echo $_ACR_PASSWORD
