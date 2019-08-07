40b22050796fc8f12cd4072b757be7069f35360c
Ben Coleman <benc.uk@gmail.com>
2019-08-07 13:50:11 +0100

    Putting Orleans back!

---
+++ orleans/Silo/Program.cs
@@ -0,0 +1,74 @@
+using Grains;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.Runtime.Configuration;
+using Orleans.Persistence.AzureStorage;
+using System;
+using System.Net;
+using System.Runtime.Loader;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Silo
+{
+  public class Program
+  {
+    private static ISiloHost silo;
+    private static readonly ManualResetEvent siloStopped = new ManualResetEvent(false);
+
+    static void Main(string[] args)
+    {
+       Console.WriteLine("##### Starting Silo...");
+
+      // get the config data needed for Orleans silo. We tend tend to favour Azure table storage, and include a sample appsettings.Sample.json file.
+      // for more info on configuration in ASP.NET Core see https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?tabs=basicconfiguration 
+      var appSettingsBuilder = new ConfigurationBuilder()
+          .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+          .AddEnvironmentVariables();
+      IConfigurationRoot appSettings = appSettingsBuilder.Build();
+
+      // start the silo
+      // see https://dotnet.github.io/orleans/Documentation/Deployment-and-Operations/Configuration-Guide/Typical-Configurations.html 
+      silo = new SiloHostBuilder()
+          .Configure<ClusterOptions>(options =>
+          {
+              options.ClusterId = appSettings["Orleans:ClusterId"];
+              options.ServiceId = appSettings["Orleans:ServiceId"];
+          })
+          .UseAzureStorageClustering(options => options.ConnectionString = appSettings["Orleans:ConnectionString"])
+          .UseAzureTableReminderService(options => options.ConnectionString = appSettings["Orleans:ConnectionString"])
+          .AddAzureTableGrainStorage("grain-store", options => options.ConnectionString = appSettings["Orleans:ConnectionString"])
+          .ConfigureEndpoints(siloPort: 11111, gatewayPort: 30000)
+          .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(EventGrain).Assembly).WithReferences())
+          .ConfigureLogging(builder => builder.SetMinimumLevel((LogLevel)appSettings.GetValue<int>("Orleans:LogLevel")).AddConsole())
+          .Build();
+
+      Task.Run(StartSilo);
+
+      AssemblyLoadContext.Default.Unloading += context =>
+      {
+        Task.Run(StopSilo);
+        siloStopped.WaitOne();
+      };
+
+      siloStopped.WaitOne();
+    }
+
+    private static async Task StartSilo()
+    {
+      await silo.StartAsync();
+      Console.WriteLine("##### Silo started!");
+    }
+
+    private static async Task StopSilo()
+    {
+      await silo.StopAsync();
+      Console.WriteLine("##### Silo stoped!");
+      siloStopped.Set();
+    }
+  }
+}
