5c9aa0850dba3af382b4907427ac29bbd24ec8eb
Ben Coleman <benc.uk@gmail.com>
2019-08-11 15:46:00 +0100

    Finally got Orleans to work in Kubernetes

--- orleans/Silo/Program.cs
+++ orleans/Silo/Program.cs
@@ -3,14 +3,13 @@ using Microsoft.Extensions.Logging;
 using Orleans;
 using Orleans.Configuration;
 using Orleans.Hosting;
-using Orleans.Runtime.Configuration;
-using Orleans.Persistence.AzureStorage;
 using System;
 using System.Net;
 using System.Runtime.Loader;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using System.Net.Sockets;
 
 namespace Silo
 {
@@ -21,7 +20,9 @@ namespace Silo
 
     static void Main(string[] args)
     {
-       Console.WriteLine("##### Starting Silo...");
+      Console.WriteLine("##### Starting Silo...");
+      var siloPort = 11111;
+      var gatewayPort = 30000;
 
       // get the config data needed for Orleans silo. We tend tend to favour Azure table storage, and include a sample appsettings.Sample.json file.
       // for more info on configuration in ASP.NET Core see https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?tabs=basicconfiguration 
@@ -31,18 +32,28 @@ namespace Silo
           .AddEnvironmentVariables();
       IConfigurationRoot appSettings = appSettingsBuilder.Build();
 
+      Console.WriteLine(appSettings["Orleans:AdvertisedIPAddress"]);
+
       // start the silo
       // see https://dotnet.github.io/orleans/Documentation/Deployment-and-Operations/Configuration-Guide/Typical-Configurations.html 
       silo = new SiloHostBuilder()
-          .Configure<ClusterOptions>(options =>
-          {
+          .Configure<ProcessExitHandlingOptions>(options => {
+              options.FastKillOnProcessExit = true;
+          })
+          .Configure<ClusterOptions>(options => {
               options.ClusterId = appSettings["Orleans:ClusterId"];
               options.ServiceId = appSettings["Orleans:ServiceId"];
           })
+          .Configure<SiloOptions>(options => {
+              options.SiloName = System.Environment.MachineName;                            
+          })
           .UseAzureStorageClustering(options => options.ConnectionString = appSettings["Orleans:ConnectionString"])
           .UseAzureTableReminderService(options => options.ConnectionString = appSettings["Orleans:ConnectionString"])
-          .AddAzureTableGrainStorage("grain-store", options => options.ConnectionString = appSettings["Orleans:ConnectionString"])
-          .ConfigureEndpoints(siloPort: 11111, gatewayPort: 30000)
+          .AddAzureTableGrainStorage("grain-store", options => {
+              options.ConnectionString = appSettings["Orleans:ConnectionString"];
+          })
+          // last param is important it enables listening on all interfaces
+          .ConfigureEndpoints(siloPort, gatewayPort, AddressFamily.InterNetwork, true)
           .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(EventGrain).Assembly).WithReferences())
           .ConfigureLogging(builder => builder.SetMinimumLevel((LogLevel)appSettings.GetValue<int>("Orleans:LogLevel")).AddConsole())
           .Build();
@@ -72,3 +83,16 @@ namespace Silo
     }
   }
 }
+
+/*
+          .Configure<EndpointOptions>(options => {
+            options.SiloPort = 11111;
+            options.GatewayPort = 30000;
+            if(appSettings["Orleans:AdvertisedIPAddress"] != null) {
+              options.AdvertisedIPAddress = IPAddress.Parse(appSettings["Orleans:AdvertisedIPAddress"]);
+            };
+            
+            //options.AdvertisedIPAddress = IPAddress.Parse("172.16.0.42");
+          })
+          
+           */
