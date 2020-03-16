7f9b61725f0e0cb24fe08ae970cc5441f781beeb
Ben Coleman <benc.uk@gmail.com>
2020-03-16 18:36:33 +0000

    Linting update

--- node/data-api/controllers/health-controller.js
+++ node/data-api/controllers/health-controller.js
@@ -1,7 +1,7 @@
-const Controller = require('./controller');
-const mongoose = require ('mongoose');
-const os = require ('os');
-const fs = require ('fs');
+const Controller = require('./controller')
+const mongoose = require('mongoose')
+const os = require('os')
+const fs = require('fs')
 
 //
 // Health controller has no backend DB model or state
@@ -9,28 +9,28 @@ const fs = require ('fs');
 // See this IETF draft spec https://tools.ietf.org/html/draft-inadarei-api-health-check
 //
 class HealthController extends Controller {
-  constructor(service) { 
-    super(service);
+  constructor(service) {
+    super(service)
   }
 
   // GET - Return health information as JSON
   async get(req, res) {
     try {
-      let packageJson = require('../package.json');
-      let status = 'pass';
-      let output = '';
-      let code = 200;
+      let packageJson = require('../package.json')
+      let status = 'pass'
+      let output = ''
+      let code = 200
 
       // Debug contains a lot of info you might not want public
       // If you're paranoid, enable when in non-production env
-      let showDebug = true; //process.env.NODE_ENV != 'production'
+      let showDebug = true //process.env.NODE_ENV != 'production'
 
       // Health logic based on MongoDB connection state
-      if(mongoose.connection.readyState != 1) {
-        status = 'fail';
-        output = `Database is not connected! State: ${mongoose.STATES[mongoose.connection.readyState]}`;
-        code = 503;
-      } 
+      if (mongoose.connection.readyState != 1) {
+        status = 'fail'
+        output = `Database is not connected! State: ${mongoose.STATES[mongoose.connection.readyState]}`
+        code = 503
+      }
 
       // Response body based on this draft spec https://tools.ietf.org/html/draft-inadarei-api-health-check
       let resp = {
@@ -38,12 +38,12 @@ class HealthController extends Controller {
         version:     packageJson.version,
         releaseID:   process.env.RELEASE_INFO || 'No release info',
         output:      output,
-        description: packageJson.name,        
+        description: packageJson.name,
       }
 
       // I extend it slightly with the debug property
-      if(showDebug) {
-        resp.debug = { 
+      if (showDebug) {
+        resp.debug = {
           hostInfo: {
             hostname:  os.hostname(),
             osType:    os.type(),
@@ -62,16 +62,16 @@ class HealthController extends Controller {
             processPath:  process.title,
             memoryRssMB:  process.memoryUsage().rss / 1048576,
             processUid:   process.getuid(),
-            inContainer:  fs.existsSync('/.dockerenv'), 
+            inContainer:  fs.existsSync('/.dockerenv'),
             inKubernetes: fs.existsSync('/var/run/secrets/kubernetes.io')
-          }          
+          }
         }
       }
 
-      // More debug info but for the DB 
-      if(showDebug && mongoose.connection.readyState == 1) {
-        var admin = await new mongoose.mongo.Admin(mongoose.connection.db);
-        let mongoBuildInfo = await admin.buildInfo();
+      // More debug info but for the DB
+      if (showDebug && mongoose.connection.readyState == 1) {
+        let admin = await new mongoose.mongo.Admin(mongoose.connection.db)
+        let mongoBuildInfo = await admin.buildInfo()
         resp.debug.mongoInfo = {
           version: mongoBuildInfo.version,
           host:    mongoose.connection.host,
@@ -80,26 +80,26 @@ class HealthController extends Controller {
         }
       }
 
-      this._sendData(res, resp, code);
-    } catch(err) {
-      this._sendError(res, err);
+      this._sendData(res, resp, code)
+    } catch (err) {
+      this._sendError(res, err)
     }
-  }  
+  }
 }
 
-// Private util to convert seconds to DD:HH:MM:SS 
-function _convertSeconds(n)  { 
-  let days = Math.floor(n / (24 * 3600)); 
-  n = n % (24 * 3600); 
-  let hours = Math.floor(n / 3600); 
-  n %= 3600; 
-  let mins = Math.floor(n / 60); 
-  n %= 60; 
-  let secs = n;
-  return `${days} days, ${hours} hours, ${mins} mins, ${secs} seconds`;
-} 
+// Private util to convert seconds to DD:HH:MM:SS
+function _convertSeconds(n)  {
+  let days = Math.floor(n / (24 * 3600))
+  n = n % (24 * 3600)
+  let hours = Math.floor(n / 3600)
+  n %= 3600
+  let mins = Math.floor(n / 60)
+  n %= 60
+  let secs = n
+  return `${days} days, ${hours} hours, ${mins} mins, ${secs} seconds`
+}
 
-module.exports = HealthController;
+module.exports = HealthController
 
 // ===== OpenAPI / Swagger generator comments below  =====
 
@@ -118,7 +118,7 @@ module.exports = HealthController;
  * @property {string} version.required - Version number - eg: 6.2.0
  * @property {string} releaseID - Additional release information
  * @property {string} description - Description of the API - eg: smilr-data-api
- * @property {object} debug - Further runtime information 
+ * @property {object} debug - Further runtime information
  * @property {object} debug.hostInfo - Details of the host
  * @property {object} debug.nodeInfo - Details of the Node version
  * @property {object} debug.runtimeInfo - Details of the runtime environment
--- node/data-api/core/database.js
+++ node/data-api/core/database.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 //
 // Creates connection to MongoDB
@@ -9,22 +9,22 @@ class DatabaseConnection {
   constructor(mongoUrl, connectTimeout = 30000) {
     let DATABASE_NAME = process.env.MONGO_DB_NAME || 'smilrDb'
 
-    console.log(`### Connecting to MongoDB: ${mongoUrl}`);
-    console.log(`### With database: ${DATABASE_NAME}`);
-    
+    console.log(`### Connecting to MongoDB: ${mongoUrl}`)
+    console.log(`### With database: ${DATABASE_NAME}`)
+
     const options = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      // Sets how long to try establishing initial server connection. Undocumented! 
-      serverSelectionTimeoutMS: connectTimeout ,
+      // Sets how long to try establishing initial server connection. Undocumented!
+      serverSelectionTimeoutMS: connectTimeout,
       dbName: DATABASE_NAME
     }
-    
-    mongoose.pluralize(null);
+
+    mongoose.pluralize(null)
 
     // Note, return the *promise* from .connect()
     return mongoose.connect(mongoUrl, options)
   }
 }
 
-module.exports = DatabaseConnection;
+module.exports = DatabaseConnection
