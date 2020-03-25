d441390050a15997f3ac3a2b34d8f2b7c1cc7206
Ben Coleman <benc.uk@gmail.com>
2020-03-25 16:27:53 +0000

    fixing unit tests

--- vue/tests/unit/EventList.spec.js
+++ vue/tests/unit/EventList.spec.js
@@ -8,47 +8,47 @@ import BootstrapVue from 'bootstrap-vue'
 localVue.use(BootstrapVue);
 
 describe('EventList.vue', () => {
-  it('renders events correctly', async () => {   
+  it('renders events correctly', () => {   
     const wrapper = shallowMount(EventList, {
       localVue,
       propsData: {
-        filter: "past"
+        filter: 'past'
       }
     })
 
-    let title = "Not real event"
+    let title = 'Not real event'
     wrapper.setData({
       events: [
         {
           title: title, 
-          type: "lab", 
-          start: "2020-01-01", 
+          _id: 'fake1',
+          type: 'lab', 
+          start: '2020-01-01', 
+          end: '2020-02-01', 
           topics: [
-            { desc: "Topic A", id: 1 },
-            { desc: "Topic B", id: 2 } 
+            { desc: 'Topic A', id: '1' },
+            { desc: 'Topic B', id: '2' } 
           ]
         },
         {
-          title: "dummy", 
-          type: "event", 
-          start: "2020-01-01", 
+          title: 'dummy', 
+          _id: 'fake2',
+          type: 'event', 
+          start: '2020-01-01', 
+          end: '2020-02-01', 
           topics: [
-            { desc: "Topic A", id: 1 },
-            { desc: "Topic B", id: 2 } 
+            { desc: 'Topic A', id: '1' },
+            { desc: 'Topic B', id: '2' } 
           ]
-        }        
+        }         
       ]
     })
         
-    await wrapper.vm.$nextTick()
-    console.log(wrapper.html());
-    
     expect(wrapper.findAll('h2').length).to.equal(2);
     expect(wrapper.find('h2').html()).to.contain(title);
     expect(wrapper.find('h2').html()).to.contain('icon="flask"');
-    // expect(wrapper.find('ul').findAll('li').length).to.equal(2);
-    expect(wrapper.find('ul').html()).to.contain("Topic A");
-    expect(wrapper.find('ul').html()).to.contain("Topic B");
-    expect(wrapper.find('b-card-stub').html()).to.contain("January 1st 2020");
+    expect(wrapper.find('ul').findAll('li').length).to.equal(2);
+    expect(wrapper.find('ul').html()).to.contain('Topic A');
+    expect(wrapper.find('b-card-stub').html()).to.contain('January 1st 2020');
   })
 })
