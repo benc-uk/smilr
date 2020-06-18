64018e777b87fab0c90f9c0d70e8ebaf09e0bd4a
Ben Coleman <benc.uk@gmail.com>
2020-06-18 12:43:11 +0100

    fixing broken unit test for EventList

--- vue/tests/unit/EventList.spec.js
+++ vue/tests/unit/EventList.spec.js
@@ -8,47 +8,48 @@ import BootstrapVue from 'bootstrap-vue'
 localVue.use(BootstrapVue);
 
 describe('EventList.vue', () => {
-  it('renders events correctly', () => {   
+  it('renders events correctly', async () => {   
+    let title = 'Not real event'
     const wrapper = shallowMount(EventList, {
       localVue,
       propsData: {
-        filter: 'past'
+        filter: 'future'
+      },
+      data() {
+        return {
+          events: [
+            {
+              title: title, 
+              _id: 'fake1',
+              type: 'lab', 
+              start: '2025-01-01', 
+              end: '2025-02-01', 
+              topics: [
+                { desc: 'Topic A', id: '1' },
+                { desc: 'Topic B', id: '2' } 
+              ]
+            },
+            {
+              title: 'dummy', 
+              _id: 'fake2',
+              type: 'event', 
+              start: '2025-01-01', 
+              end: '2025-02-01', 
+              topics: [
+                { desc: 'Topic A', id: '1' },
+                { desc: 'Topic B', id: '2' } 
+              ]
+            }         
+          ]
+        }
       }
     })
 
-    let title = 'Not real event'
-    wrapper.setData({
-      events: [
-        {
-          title: title, 
-          _id: 'fake1',
-          type: 'lab', 
-          start: '2020-01-01', 
-          end: '2020-02-01', 
-          topics: [
-            { desc: 'Topic A', id: '1' },
-            { desc: 'Topic B', id: '2' } 
-          ]
-        },
-        {
-          title: 'dummy', 
-          _id: 'fake2',
-          type: 'event', 
-          start: '2020-01-01', 
-          end: '2020-02-01', 
-          topics: [
-            { desc: 'Topic A', id: '1' },
-            { desc: 'Topic B', id: '2' } 
-          ]
-        }         
-      ]
-    })
-        
     expect(wrapper.findAll('h2').length).to.equal(2);
     expect(wrapper.find('h2').html()).to.contain(title);
     expect(wrapper.find('h2').html()).to.contain('icon="flask"');
     expect(wrapper.find('ul').findAll('li').length).to.equal(2);
     expect(wrapper.find('ul').html()).to.contain('Topic A');
-    expect(wrapper.find('b-card-stub').html()).to.contain('January 1st 2020');
+    expect(wrapper.find('b-card-stub').html()).to.contain('January 1st 2025');
   })
 })
